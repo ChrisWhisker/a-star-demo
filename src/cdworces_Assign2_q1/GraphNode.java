c074b63957cd20f50ad9406cdb53e150c99f1c43
Chris Worcester <chrisworc@gmail.com>
2024-02-08 12:41:01 -0500

    Add getters, bi-directional edges, and to string method

--- src/cdworces_Assign2_q1/GraphNode.java
+++ src/cdworces_Assign2_q1/GraphNode.java
@@ -12,21 +12,51 @@ import java.util.Map;
  */
 public class GraphNode {
 
-	String name;
-	int heuristic;
+	private String name;
+	private int heuristic;
 	// neighboring cities and their distances
-	Map<GraphNode, Integer> neighbors = new HashMap<>();
+	private Map<GraphNode, Integer> neighbors = new HashMap<>();
 
 	/**
-	 * 
+	 * Constructor
 	 */
 	public GraphNode(String name, int heuristic) {
 		this.name = name;
 		this.heuristic = heuristic;
 	}
 
-	public void AddNeighbor(GraphNode neighborNode, int distance) {
-		this.neighbors.put(neighborNode, distance);
+	// Getters
+	public String getName() {
+		return name;
+	}
+
+	public int getHeuristic() {
+		return heuristic;
+	}
+
+	public Map<GraphNode, Integer> getNeighbors() {
+		return neighbors;
+	}
+
+	// Add bi-directional edge between this node and other
+	public void addNeighbor(GraphNode other, int distance) {
+		if (this.neighbors.containsKey(other)) {
+			return;
+		}
+		this.neighbors.put(other, distance);
+		other.addNeighbor(this, distance);
+	}
+
+	@Override
+	public String toString() {
+		String s = "GraphNode [name=" + name + ", heuristic=" + heuristic + ", neighbors={ ";
+
+		for (Map.Entry<GraphNode, Integer> neighbor : neighbors.entrySet()) {
+			s = s + "\n\t[name=" + neighbor.getKey().name + ", distance=" + neighbor.getValue() + "]";
+		}
+
+		s = s + "\n}]";
+		return s;
 	}
 
 }
